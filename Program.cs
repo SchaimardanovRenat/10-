a17d3b6fc1d714efd7f5b6b490fc4c87ea07e87c
SchaimardanovRenat <145016510+SchaimardanovRenat@users.noreply.github.com>
2023-12-25 23:55:44 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,153 +1,155 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
-
-namespace ConsoleExplorer
+﻿public class Administrator
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string currentPath = "";
-            int selectedIndex = 0;
+    public string Login { get; set; }
+    public string Password { get; set; }
+    // другие свойства и методы администратора
+}
 
-            while (true)
+public class Cashier
+{
+    public string Login { get; set; }
+    public string Password { get; set; }
+    // другие свойства и методы кассира
+}
+
+public class PersonnelManager
+{
+    public static Dictionary<string, string> UserToEmployeeMap { get; set; }
+
+    static PersonnelManager()
+    {
+        UserToEmployeeMap = new Dictionary<string, string>();
+    }
+
+    public static void AddMapping(string userLogin, string employeeName)
+    {
+        if (!UserToEmployeeMap.ContainsKey(userLogin))
+        {
+            UserToEmployeeMap.Add(userLogin, employeeName);
+        }
+        else
+        {
+            UserToEmployeeMap[userLogin] = employeeName;
+        }
+    }
+
+    // другие методы
+}
+
+public class InformationSystem
+{
+    public void Run()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Добро пожаловать в информационную систему магазина!");
+
+            Console.Write("Введите логин: ");
+            string login = Console.ReadLine();
+
+            Console.Write("Введите пароль: ");
+            string password = GetHiddenPassword();
+
+            if (Authenticate(login, password))
             {
                 Console.Clear();
+                Console.WriteLine("Авторизация прошла успешно!\n");
+                Console.WriteLine($"Вы вошли под логином {GetLoggedInUsername(login)}.\n");
 
-                if (currentPath == "")
+                // Здесь вызывайте соответствующее меню в зависимости от роли
+                if (PersonnelManager.UserToEmployeeMap.ContainsKey(login))
                 {
-                    Console.WriteLine("Drive Selection:");
-                    string[] drives = Directory.GetLogicalDrives();
-                    for (int i = 0; i < drives.Length; i++)
+                    string role = PersonnelManager.UserToEmployeeMap[login];
+                    switch (role)
                     {
-                        if (i == selectedIndex)
-                        {
-                            Console.WriteLine($"> {drives[i]}");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"  {drives[i]}");
-                        }
-                    }
-
-                    Console.WriteLine("Enter Q to quit.");
-                }
-                else
-                {
-                    Console.WriteLine($"Current Path: {currentPath}");
-                    Console.WriteLine();
-
-                    string[] directories = Directory.GetDirectories(currentPath);
-                    string[] files = Directory.GetFiles(currentPath);
-
-                    for (int i = 0; i < directories.Length; i++)
-                    {
-                        string dirName = Path.GetFileName(directories[i]);
-                        string creationDate = Directory.GetCreationTime(directories[i]).ToString();
-                        if (i == selectedIndex)
-                        {
-                            Console.WriteLine($"> {dirName} [DIR]        Created: {creationDate}");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"  {dirName} [DIR]        Created: {creationDate}");
-                        }
-                    }
-
-                    for (int i = 0; i < files.Length; i++)
-                    {
-                        string fileName = Path.GetFileName(files[i]);
-                        string fileExtension = Path.GetExtension(files[i]);
-                        string creationDate = File.GetCreationTime(files[i]).ToString();
-                        if (i + directories.Length == selectedIndex)
-                        {
-                            Console.WriteLine($"> {fileName}        Extension: {fileExtension}        Created: {creationDate}");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"  {fileName}        Extension: {fileExtension}        Created: {creationDate}");
-                        }
-                    }
-
-                    Console.WriteLine();
-                    Console.WriteLine("Enter 0 to go up, R to refresh, or Q to quit.");
-                }
-
-                ConsoleKeyInfo keyInfo = Console.ReadKey();
-                Console.WriteLine();
-
-                if (keyInfo.Key == ConsoleKey.Q)
-                {
-                    break;
-                }
-                else if (keyInfo.Key == ConsoleKey.R)
-                {
-                    continue;
-                }
-
-                if (currentPath == "")
-                {
-                    if (keyInfo.Key == ConsoleKey.DownArrow)
-                    {
-                        selectedIndex = Math.Min(selectedIndex + 1, Directory.GetLogicalDrives().Length - 1);
-                    }
-                    else if (keyInfo.Key == ConsoleKey.UpArrow)
-                    {
-                        selectedIndex = Math.Max(selectedIndex - 1, 0);
-                    }
-                    else if (keyInfo.Key == ConsoleKey.Enter)
-                    {
-                        string[] drives = Directory.GetLogicalDrives();
-                        currentPath = drives[selectedIndex];
+                        case "администратор":
+                            ShowAdministratorMenu();
+                            break;
+                        case "кассир":
+                            ShowCashierMenu();
+                            break;
+                        // Добавьте вызов методов для остальных должностей
+                        default:
+                            Console.WriteLine("Роль не определена.");
+                            break;
                     }
                 }
                 else
                 {
-                    if (keyInfo.Key == ConsoleKey.D0 || keyInfo.Key == ConsoleKey.NumPad0)
-                    {
-                        currentPath = Directory.GetParent(currentPath)?.FullName ?? "";
-                    }
-                    else if (keyInfo.Key == ConsoleKey.DownArrow)
-                    {
-                        selectedIndex = Math.Min(selectedIndex + 1, Directory.GetDirectories(currentPath).Length + Directory.GetFiles(currentPath).Length - 1);
-                    }
-                    else if (keyInfo.Key == ConsoleKey.UpArrow)
-                    {
-                        selectedIndex = Math.Max(selectedIndex - 1, 0);
-                    }
-                    else if (keyInfo.Key == ConsoleKey.Enter)
-                    {
-                        string[] directories = Directory.GetDirectories(currentPath);
-                        string[] files = Directory.GetFiles(currentPath);
-                        if (selectedIndex < directories.Length)
-                        {
-                            currentPath = directories[selectedIndex];
-                        }
-                        else if (selectedIndex < directories.Length + files.Length)
-                        {
-                            string fileName = files[selectedIndex - directories.Length];
-                            string extension = Path.GetExtension(fileName);
-
-                            if (extension == ".txt")
-                            {
-                                Process.Start("notepad.exe", fileName);
-                                Console.WriteLine($"Opening file: {fileName} in Notepad");
-                            }
-                            else if (extension == ".docx")
-                            {
-                                Process.Start("winword.exe", fileName);
-                                Console.WriteLine($"Opening file: {fileName} in Word");
-                            }
-                            else
-                            {
-                                Console.WriteLine($"Unsupported file type: {extension}");
-                            }
-                        }
-                    }
+                    Console.WriteLine("Роль не определена.");
                 }
 
+                break;
+            }
+            else
+            {
+                Console.WriteLine("\nОшибка авторизации. Попробуйте еще раз.\n");
             }
         }
+
+        Console.WriteLine("Нажмите любую клавишу для выхода.");
+        Console.ReadKey();
+    }
+
+    private string GetHiddenPassword()
+    {
+        string password = "";
+        ConsoleKeyInfo key;
+
+        do
+        {
+            key = Console.ReadKey(true);
+
+            if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+            {
+                password += key.KeyChar;
+                Console.Write("*");
+            }
+            else if (key.Key == ConsoleKey.Backspace)
+            {
+                if (!string.IsNullOrEmpty(password))
+                {
+                    password = password.Substring(0, password.Length - 1);
+                    Console.Write("\b \b");
+                }
+            }
+        } while (key.Key != ConsoleKey.Enter);
+
+        Console.WriteLine();
+
+        return password;
+    }
+
+    private bool Authenticate(string login, string password)
+    {
+        // Реализуйте метод аутентификации с использованием введенных логина и пароля
+        return true; // Верните true, если аутентификация успешна; иначе верните false
+    }
+
+    private string GetLoggedInUsername(string login)
+    {
+        // Получите имя пользователя для указанного логина
+        return ""; // Замените пустую строку на соответствующий код
+    }
+
+    private void ShowAdministratorMenu()
+    {
+        // Реализуйте логику меню администратора
+    }
+
+    private void ShowCashierMenu()
+    {
+        // Реализуйте логику меню кассира
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        InformationSystem system = new InformationSystem();
+        system.Run();
     }
 }
