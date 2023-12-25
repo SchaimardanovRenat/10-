2be755c5cf2f8a8106f9f57db1a467bf6a0f42b6
SchaimardanovRenat <145016510+SchaimardanovRenat@users.noreply.github.com>
2023-12-25 23:58:19 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,14 +2,13 @@
 {
     public string Login { get; set; }
     public string Password { get; set; }
-    // другие свойства и методы администратора
 }
 
 public class Cashier
 {
     public string Login { get; set; }
     public string Password { get; set; }
-    // другие свойства и методы кассира
+    
 }
 
 public class PersonnelManager
@@ -33,7 +32,7 @@ public class PersonnelManager
         }
     }
 
-    // другие методы
+  
 }
 
 public class InformationSystem
@@ -57,7 +56,7 @@ public class InformationSystem
                 Console.WriteLine("Авторизация прошла успешно!\n");
                 Console.WriteLine($"Вы вошли под логином {GetLoggedInUsername(login)}.\n");
 
-                // Здесь вызывайте соответствующее меню в зависимости от роли
+                
                 if (PersonnelManager.UserToEmployeeMap.ContainsKey(login))
                 {
                     string role = PersonnelManager.UserToEmployeeMap[login];
@@ -69,7 +68,7 @@ public class InformationSystem
                         case "кассир":
                             ShowCashierMenu();
                             break;
-                        // Добавьте вызов методов для остальных должностей
+                        
                         default:
                             Console.WriteLine("Роль не определена.");
                             break;
@@ -123,24 +122,20 @@ public class InformationSystem
 
     private bool Authenticate(string login, string password)
     {
-        // Реализуйте метод аутентификации с использованием введенных логина и пароля
-        return true; // Верните true, если аутентификация успешна; иначе верните false
     }
 
     private string GetLoggedInUsername(string login)
     {
-        // Получите имя пользователя для указанного логина
-        return ""; // Замените пустую строку на соответствующий код
+        
     }
 
     private void ShowAdministratorMenu()
     {
-        // Реализуйте логику меню администратора
-    }
+      }
 
     private void ShowCashierMenu()
     {
-        // Реализуйте логику меню кассира
+        
     }
 }
 
